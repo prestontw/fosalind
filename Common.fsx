f9eab8870f8b72fa7e302a55c5301f8e2b679f4c
Preston Tunnell Wilson <preston_tunnell_wilson@brown.edu>
2022-09-19 23:59:43 -0400

    Ptw/finish fib and gc
    * Finish fib
    
    * Move parsing fasta to common
    
    * Finish gc

--- Common.fsx
+++ Common.fsx
@@ -17,7 +17,12 @@ let NucleobaseOfChar =
     | _ -> None
 
 let readData fileName =
-    System.IO.File.ReadLines("data/" + fileName)
+    System.IO.File.ReadAllText("data/" + fileName)
+
+let readFirstLine filename : string =
+    filename
+    |> readData
+    |> fun s -> s.Split "\n"
     |> Seq.head
 
 module RNA =
@@ -26,3 +31,30 @@ module RNA =
         | C
         | G
         | U
+
+type FastaRecord = { id: string; body: string }
+
+let parseFasta (s: string) =
+    let lines = s.Split "\n"
+
+    let output =
+        Array.fold
+            (fun (list, current) (line: string) ->
+                match Seq.tryHead line, current with
+                | None, Some current -> Array.append list [| current |], None
+                | Some '>', Some previous -> Array.append list [| previous |], Some { id = line[1..]; body = "" }
+                | Some '>', None -> list, Some { id = line[1..]; body = "" }
+                | Some c, Some current -> list, Some { current with body = current.body + line }
+                // shouldn't happen, really
+                | None, None -> list, None
+                | Some _c, None -> list, None)
+            ([||], None)
+            (lines)
+
+    match output with
+    | ret, Some remainder -> Array.append ret [| remainder |]
+    | ret, None -> ret
+
+let tee x =
+    printfn "%A" x
+    x
