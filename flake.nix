65c01d90274cdbb343a9ebb9b2258cb840f4ffe3
Preston TW <preston_tunnell_wilson@brown.edu>
2022-11-09 01:15:07 -0500

    Update url
    I would think that the other one would work, though!

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
     utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, utils, ... }:
